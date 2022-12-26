f9e20f98a40b14ae455ab3f77359c97b6ced023a
Ahmad Mohammed Abdelhameed <abdelhameed9@yahoo.com>
2022-12-27 01:45:57 +0200

    adding bootstrap pagination using publish vendor

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,22 +8,26 @@ Home
 			<div class="container">
 				<div class="row">
 					<div class="col-md-8 posts-col">
-
-@foreach ($posts as $post)
-<div class="block-21 d-flex animate-box post">
-    <a href="#" class="blog-img" style="background-image: url({{asset('storage/'.$post->image->path)}});"></a>
-    <div class="text">
-    <h3 class="heading"><a href="#">{{$post->title}}</a></h3>
-    <p class="excerpt">{{$post->excerpt}}</p>
-    <div class="meta">
-        {{-- <div><a href="#"><span class="icon-calendar"></span>{{$post->created_at}}</a></div> --}}
-        <div><a class="date" href="#"><span class="icon-calendar"></span>{{$post->created_at->diffForHumans()}}</a></div>
-        <div><a href="#"><span class="icon-user2"></span> {{$post->author->name}}</a></div>
-        <div class="comments_count"><a href="#"><span class="icon-chat"></span> {{$post->comments_count}}</a></div>
-    </div>
-    </div>
-</div>
-@endforeach
+                        @if (! count($posts))
+                            <p class="lead"> There are no posts to show</p>
+                        @else
+                        @foreach ($posts as $post)
+                        <div class="block-21 d-flex animate-box post">
+                            <a href="#" class="blog-img" style="background-image: url({{asset('storage/'.$post->image->path)}});"></a>
+                            <div class="text">
+                            <h3 class="heading"><a href="#">{{$post->title}}</a></h3>
+                            <p class="excerpt">{{$post->excerpt}}</p>
+                            <div class="meta">
+                                {{-- <div><a href="#"><span class="icon-calendar"></span>{{$post->created_at}}</a></div> --}}
+                                <div><a class="date" href="#"><span class="icon-calendar"></span>{{$post->created_at->diffForHumans()}}</a></div>
+                                <div><a href="#"><span class="icon-user2"></span> {{$post->author->name}}</a></div>
+                                <div class="comments_count"><a href="#"><span class="icon-chat"></span> {{$post->comments_count}}</a></div>
+                            </div>
+                            </div>
+                        </div>
+                        @endforeach
+                        @endif
+                        {{$posts -> links()}}
 
 
 
